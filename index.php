874b119a541df2d9dbbf8f59a92909c79b6e2b35
OkaniYoshiii <dragut31620@gmail.com>
2024-08-31 20:20:25 +0200

    Traduction + mise en cache

--- index.php
+++ index.php
@@ -1,23 +1,56 @@
 <?php
+$cacheFilename = 'cache.json';
+if(!file_exists($cacheFilename)) {
+    $xml = simplexml_load_file('items.xml');
 
-$xml = simplexml_load_file('items.xml');
-
-// $items = $xml->xpath('/items/item/property[@name="Tags" and contains(@value, "axe")]/..');
-$items = $xml->xpath('/items/item/property[@name="EconomicValue"]/..');
-
-$items = array_map(fn($item) => ['name' => (string) $item['name'], 'economicValue' => (string) $item->xpath('property[@name="EconomicValue"]/@value')[0]['value']], $items);
-// var_dump($items);
+    // $items = $xml->xpath('/items/item/property[@name="Tags" and contains(@value, "axe")]/..');
+    $items = $xml->xpath('/items/item/property[@name="EconomicValue"]/..');
+    
+    $itemsFiltered = [];
+    foreach($items as $item)
+    {
+        $name = (string) $item['name'];
+        $economicValue = (string) $item->xpath('property[@name="EconomicValue"]/@value')[0]['value'];
+        $itemsFiltered[$name] = ['economicValue' => $economicValue, 'locale' => ['code' => $name]];
+    }
+    
+    $localizationResource = fopen('Localization.txt', 'r');
+    
+    $linesCount = 1;
+    $lines[1] = fgetcsv($localizationResource, 1000);
+    $keyPos = 0;
+    $frenchPos = array_search('french', $lines[1]);
+    $englishPos = array_search('english', $lines[1]);
+    
+    while(($line = fgetcsv($localizationResource)) !== false)
+    {
+        $linesCount++;
+    
+        $key = $line[$keyPos];
+    
+        if(isset($itemsFiltered[$key])) {
+            $itemsFiltered[$key]['locale']['fr'] = $line[$frenchPos];
+            $itemsFiltered[$key]['locale']['en'] = $line[$englishPos];
+        }
+    }
+    
+    fclose($localizationResource);
+} else {
+    $itemsFiltered = json_decode(file_get_contents($cacheFilename), true);
+}
 
 echo '<div style="max-width: 900px; margin-inline: auto;">';
 echo '<h1>Items et leurs prix : </h1>';
 
-foreach($items as $item)
+foreach($itemsFiltered as $name => $item)
 {
     echo '<div>';
-    echo '<p>Nom de l\'item : ' . $item['name'] . '</p>';
+    echo '<p>Noms de l\'item (code, FR, EN) : ' . implode(', ', $item['locale']) . '</p>';
     echo '<p>Prix de base de l\'item : ' . $item['economicValue'] . ' Dukes </p>';
     echo '</div>';
     echo '<hr>';
 }
 
 echo '</div>';
+
+file_put_contents($cacheFilename, json_encode($itemsFiltered));
